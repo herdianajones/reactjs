c1ef848ff34daa87a9721cc03c16ada608987197
herdianajones <herdiana.jones@gmail.com>
2018-09-15 23:02:57 +0700

    lesson 11 added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     const siswa = ['Andi','Budi','Caca'];
-    const listSiswa = siswa.map((siswa) => <li>{siswa}</li>);
-    
+    const listSiswa = siswa.map((siswa, index) => <li key={index}>{siswa}</li>);
+
     return (
       <div>
         <h1>Daftar Siswa:</h1>
         <h1>{listSiswa}</h1>
       </div>
-      );
-    }
+    );
+  }
 }
 
 export default App;
