99e7f666de5c702470f0dd85a07cb2c229c3c406
herdianajones <herdiana.jones@gmail.com>
2018-09-23 11:25:23 +0700

    lesson 21 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  klik(){
-    alert('Tombol sudah diklik!');
+  klik(siapa){
+    alert('Halo kakak '+siapa);
   }
   render() {
     return (
       <div>
         <h1>Selamat datang!</h1>
-        <button onClick={this.klik}>
+        <button onClick={() => {this.klik('Andi')} }>
         Coba Klik!
         </button>
       </div>
