3632de0bd40b2169ca9ef1e3b2a59a4f6544dadb
herdianajones <herdiana.jones@gmail.com>
2018-09-30 07:49:56 +0700

    lesson 31 added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 class App extends Component {
   componentDidMount(){
-    axios.get('https://facebook.github.io/react-native/movies.json')
-      .then((ambilData) => {
+    axios(
+    {
+        url: 'https://facebook.github.io/react-native/movies.json',
+        method: 'GET',
+    }).then((ambilData) => {
         console.log(ambilData.data.title);
         console.log(ambilData.data.description);
         console.log(ambilData.data.movies);
