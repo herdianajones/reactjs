826d4c556e8e002c715a99e23fecf629479cc7a7
herdianajones <herdiana.jones@gmail.com>
2018-09-30 07:42:15 +0700

    lesson 29 added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom';
-
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Article from './component/Article';
-
+import axios from 'axios';
 class App extends Component {
-
+  componentDidMount(){
+    axios.get('https://facebook.github.io/react-native/movies.json')
+      .then((ambilData) => {
+        console.log(ambilData);
+      })
+  };
+  
   render() {
     return (
       <div>
-        <h1>Coba Routing</h1>
-        <ul>
-          <li><Link to="/">Beranda</Link></li>
-          <li><Link to="/header">Header</Link></li>
-          <li><Link to="/article">Article</Link></li>
-          <li><Link to="/footer">Footer</Link></li>
-        </ul>
-        <div>
-          <Route path="/" component={'Home'}/>
-          <Route path="/article" component={Article}/>
-          <Route path="/header" component={Header}/>
-          <Route path="/footer" component={Footer}/>
-        </div>
+        <h1>Coba Get Data</h1>
       </div>
     );
   }
